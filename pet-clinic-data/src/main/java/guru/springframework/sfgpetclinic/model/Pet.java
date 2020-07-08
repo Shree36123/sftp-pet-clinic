d89d66a3b408d48320bd42a9f946464cf66ad9c3
Shree <reachoutshree@gmail.com>
2020-07-08 06:44:42 +0530

    ID Property is added. Closes #10

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet extends PetType {
+public class Pet extends BaseEntity {
+	
 	private Owner owner;
 	private PetType petType;
 	private LocalDate birthDate ;
