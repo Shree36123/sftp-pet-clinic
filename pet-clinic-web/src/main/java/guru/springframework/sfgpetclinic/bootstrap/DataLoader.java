fb4089972e37b03c3f74ab39bb39fe59b45d8030
Shree <reachoutshree@gmail.com>
2020-07-18 08:10:26 +0530

    Done with create config for server Closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -16,34 +16,33 @@ public class DataLoader implements CommandLineRunner{
 	public OwnerService ownerService;
 	public VetService vetService;
 	
-	public DataLoader(OwnerServiceMap ownerServiceMap, VetServiceMap vetServiceMap) {
-		ownerService=new OwnerServiceMap();
-		vetService=new VetServiceMap();
-		}
-
+	public DataLoader(OwnerService ownerService, VetService vetService) {
+		this.ownerService = ownerService;
+		this.vetService = vetService;
+	}
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Owner is loading ");
 		Owner owner1=new Owner();
 		owner1.setFirstName("Shree");
 		owner1.setLastName("Ranga");
-		owner1.setId(1L);
+		//owner1.setId(1L);
 		ownerService.save(owner1);
 		Owner owner2=new Owner();
 		owner2.setFirstName("2nd Owner");
 		owner2.setLastName("LastName");
-		owner2.setId(2L);
+		//owner2.setId(2L);
 		ownerService.save(owner2);
 		System.out.println("Vet is loading ");
 		Vet vet1=new Vet();
 		vet1.setFirstName("vet1");
 		vet1.setLastName("Last");
-		vet1.setId(1L);
+		//vet1.setId(1L);
 		vetService.save(vet1);
 		Vet vet2=new Vet();
 		vet2.setFirstName("vet2");
 		vet2.setLastName("Last");
-		vet2.setId(2L);
+		//vet2.setId(2L);
 		vetService.save(vet2);
 		
 	}
