de6c123948ffada35b3395272cf445bcbc157e2b
Shree <reachoutshree@gmail.com>
2020-07-18 08:12:49 +0530

    list of owner details are added Closes #16

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,12 +1,21 @@
 package guru.springframework.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import guru.springframework.sfgpetclinic.services.OwnerService;
 @Controller
 public class OwnerController {
 
+	private final OwnerService ownerservice;
+		
+	public OwnerController(OwnerService ownerservice) {
+		this.ownerservice = ownerservice;
+	}
+
 	@RequestMapping({"owners","owners/index","owners/index.html"})
-	public String listowners() {
+	public String listowners(Model model) {
+		model.addAttribute("owners",ownerservice.findAll());
 		return "owners/index";
 	}
 	
