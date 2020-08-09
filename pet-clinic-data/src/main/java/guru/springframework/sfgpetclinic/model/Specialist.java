2a3933d6284b0d027d0ecbffaaced1b3fef15d3a
Shree <reachoutshree@gmail.com>
2020-08-09 21:33:03 +0530

    created Specialist details and associated with vet Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialist.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Specialist extends BaseEntity{
+	
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,5 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
 
+	Set<Specialist> specialistes;
+
+	public Set<Specialist> getSpecialistes() {
+		return specialistes;
+	}
+
+	public void setSpecialistes(Set<Specialist> specialistes) {
+		this.specialistes = specialistes;
+	}
+	
+	
 }
