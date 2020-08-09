b68f8c038d76dd08f53466e719ce3749994b6c1c
Shree <reachoutshree@gmail.com>
2020-08-09 22:02:16 +0530

    Contact information is added to Owner Model. Closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,5 +1,37 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+
+	private String address;
+	private String street;
+	private String telephoneNumber;
+	private Set<Pet> pets;
+
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getTelephoneNumber() {
+		return telephoneNumber;
+	}
+	public void setTelephoneNumber(String telephoneNumber) {
+		this.telephoneNumber = telephoneNumber;
+	}
+	public Set<Pet> getPets() {
+		return pets;
+	}
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
 
 }
